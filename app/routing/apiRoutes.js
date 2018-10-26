9b438a7d662fd3211e6bc473e35f5bc21bf9ac0a
Jonathan Bald <baldyj117@yahoo.com>
2018-10-25 20:42:27 -0500

    finished

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,1 +1,51 @@
+var path = require("path");
+var userData = require("../data/friends");
 
+module.exports = function(app) {
+    
+    app.get("/api/friends", (req,res) => {
+        res.json(userData);
+
+    });
+
+    app.post("/api/friends", (req,res) => {
+        
+        var yourMatch = {
+            name: "",
+            photo: "",
+            friendDifference: 1000
+        };
+
+        var userData = req.body;
+        var userScores = userData.scores;
+        var userName = userData.name;
+        var userPhoto = userData.photo;
+
+        var totalDifference = 0;
+
+        for (var i=0; i < friends.length - 1; i++) {
+            console.log(friends[i].name);
+            totalDifference = 0;
+
+            for (var j=0; j < 10; j++) {
+                totalDifference += Math.abs(parseInt(userScores[j]) - parseInt(friends[i].scores[j]));
+                
+                if (totalDifference <= yourMatch.friendDifference) {
+
+                    yourMatch.name = friends[i].name;
+                    yourMatch.photo = friends[i].photo;
+                    yourMatch.friendDifference = totalDifference;
+                    
+                }
+
+            }
+        }
+
+        friends.push(userData);
+
+        res.json(yourMatch);
+
+    });
+
+
+};
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,1 +1,14 @@
+var path = require("path");
 
+module.exports = function(app) {
+
+    app.get("/survey", (req,res) => {
+        res.sendFile(path.join(__dirname, "../survey.html"));
+    });
+
+    app.get("*", (req,res) => {
+        res.sendFile(path.join(__dirname, "../home.html"));
+    });
+
+
+};
