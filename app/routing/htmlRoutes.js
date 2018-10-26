40c9d5f5523036efdb7f3a0d47ced782f50043fb
Jonathan Bald <baldyj117@yahoo.com>
2018-10-25 23:39:41 -0500

    bugfound

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -2,11 +2,13 @@ var path = require("path");
 
 module.exports = function(app) {
 
+
+
     app.get("/survey", (req,res) => {
         res.sendFile(path.join(__dirname, "../survey.html"));
     });
 
-    app.get("*", (req,res) => {
+    app.use("*", (req,res) => {
         res.sendFile(path.join(__dirname, "../home.html"));
     });
 
